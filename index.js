4b37076c52f707394d8c6b58f0a67a43e4b40996
geoiraq <george.ejaam@gmail.com>
2017-10-01 21:50:13 +1100

    adding muli-page and fixing getting promised answers

--- index.js
+++ index.js
@@ -29,75 +29,72 @@ app.get('/', function (req, res) {
   res.sendfile('./www/index.html');
 });
 
-// the form
+
+
+
 app.post('/',function(req, res){
 //go fetch questions from SO API based on the query and tag provided by user
-var getqs = require('./lib/getquestion.js');
+var getqs = require('./lib/getqspromised.js');
+
+
 
 for (i= 1; i < 3; i++){
-   getqs.getQuestions(req.body.apiName,req.body.tag,i, function(body){
-     var fc = require('./lib/filterbodyforcode.js');
-     var fdc = fc.filterResult(body);
-     var fqids = require('./lib/filterQIds.js');
-     var qids = fqids.filterQIds(body);	
-      
-//     console.log(fdc);
-     //storing the results as json file
-      var jsonresult = JSON.stringify(fdc);
-//     console.log(jsonresult); 
- //result of all code blocks in Questions
-      fs.writeFile("file1.json", jsonresult, 'utf8', function(err){
-        if (err) throw err; 
-         console.log('complete for code in questions');
-        });
-
-
-//console.log("HERE ARE QIDS " + qids);
-
-// now work on the qids to have the code out of their body content
-
-   var ga = require('./lib/getanswers.js');
-   ga.getAnswers(qids, function(err, result)
+getqs.getQuestions(req.body.apiName,req.body.tag,i)
+        .then(function(result){
+                var fc = require('./lib/filterbodyforcode.js');
+                var fdc = fc.filterResult(result);
+                var fqids = require('./lib/filterQIds.js');
+        	var qids = fqids.filterQIds(result);
+        	var jsonresult = JSON.stringify(fdc);    
+        	fs.appendFile("file1.json", jsonresult, 'utf8', function(err)
+                  {
+                    if (err) throw err;
+         	    console.log('complete for code in questions of page: '+ i);
+                  });
+               console.log(qids);
+               
+// answers promise
+answersPromise = function(qids){
+return new Promise(function(resolve, reject)
+ {
+  var ga = require('./lib/getanswers');
+  ga.getAnswers(qids, function(err, result)
       {
-      // if err throw err;// TODO handle err from err json obj
-
+      if (err) reject(err);
       var fa = require('./lib/filteransbodyforcode.js');
       var fr = fa.filterAnswersResult(result);
       var jsonresult2 = JSON.stringify(fr);
-
-//     var answerscodes = fc.filterResult(qids);
-
-     fs.writeFile("file2.json", jsonresult2, 'utf8', function(err){
-         if (err) throw err; 
-         console.log('complete.. for code in answers');
+      resolve(jsonresult2);
         });
-       
-
-
-
        }
-    ); // here comes body of all answers that belongs to the qids
-//console.log("as body " + asbody);
-
-
-    });
+    );
 }
 
 
+answersPromise(qids).then(function(jsonresult2){
+      fs.writeFile("resultfile.json", jsonresult2, 'utf8', function(err){
+      if (err) throw err
+      console.log('complete.. for code in answers')});
+               }).catch(function(){
+                //do some error handling
+      console.log("error");
+             })
 
 
 
-
-
-res.send('Completed successfuly');
+ 
+       });
+      }
 res.end();
-});
+})
+//res.send('Completed successfuly');
+
+
 
 
 app.get('/codes', function(req, res){
 fs.readFile('./file1.json', function (err, content){
  if (err) throw err;
-
  res.send(JSON.parse(content));
 });
 });
@@ -109,17 +106,5 @@ console.log(id);
 content = JSON.parse(content);
 res.send(content["items"][id]);
 
-/*
-var decodedObj = [{}];
-decodedObj.push(
-  {"tags": content["items"][id]["tags"],
-   "title": content["items"][id]["title"],
-   "code_blocks":content["items"][id]["codeblocks"]
-}
-);
-
-res.send(decodedObj);
-*/
 });
-
 app.listen(port);
