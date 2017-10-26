b4e2ba0e770ee3165a9275365e09e3ffb276e628
geoiraq <george.ejaam@gmail.com>
2017-10-27 09:02:37 +1100

    whileJs can find a method name!

--- while.js
+++ while.js
@@ -22,13 +22,58 @@ app.get('/', function (req, res) {
 
 //Get code examples based on a given API method
 app.get('/code/methods/:method_name', function(req,res){
-// Look at answers
-
+// Look at all answers with full content
 var results_file = require('./resultfile.json');
+var list_results = {answers:[]};
+var look_for = req.params.method_name; //the method that we are looking for
+
+
+//for loop
+// for (var i in results_file.items){
+//change 0 to i when TODO
 var code = results_file.items[0]['body'];
+
+var d = require('./lib/detectcode.js');
+//find any code snippets within the text
+var code1 = d.detectCode(code);
+//code1 is array of code blocks
+
 var regexp = require('./lib/detectURL.js');
-var r = regexp.detectRegExp(code);
-res.send(r);
+
+// now code 1 is an array we need to look at each code block
+for (var c in code1){
+var r = regexp.detectRegExp(code1); //returns a JSON obj with uri and any methods detected
+//console.log(JSON.stringify(r));
+//JSONPATH will filter all results that uses an api identified by it's uri
+var f = jp.query(r,'$.terms[?(@.uri)]');
+console.log(JSON.stringify(jp.query(r,'$.terms')));
+//
+console.log(f);
+// f[0].uri returns the uri and position
+//f[0].methods_terms returns all programming terms in the code
+  //search the array for a match
+  for (var item in f[0].methods_terms){
+//console will log any method detected with it's position
+// console.log(f[0].methods_terms[item]);
+  if (look_for === f[0].methods_terms[item]){
+   console.log('found ',f[0].methods_terms[item]);
+  list_results.answers.push(
+      {
+        "found":true
+      }
+     )
+   }
+  }
+//} end for loop
+
+
+
+}
+
+
+
+
+res.send(list_results);
 });
 
 
